6d5b2f798b619bf567ed327fdaac8611fe50e245
grant <117955357+Kiddeke@users.noreply.github.com>
2024-09-26 11:28:38 -0400

    Update header for mobile

--- public/script.js
+++ public/script.js
@@ -1,52 +1,3 @@
-// JavaScript File (script.js)
-
-// Animate the header logo on page load
-const headerLogo = document.querySelector('.headerlogo');
-headerLogo.style.opacity = 0;
-headerLogo.style.transform = 'translateX(-50px)';
-headerLogo.style.transition = 'opacity 1s, transform 1s';
-setTimeout(() => {
-  headerLogo.style.opacity = 1;
-  headerLogo.style.transform = 'translateX(0)';
-}, 500);
-
-
-
-
-  // DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed.
-  addEventListener('DOMContentLoaded', (event) => {
-    console.log('The DOM is fully loaded.');
-  });
-
-  // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
-  addEventListener('load', (event) => {
-    console.log('The page is fully loaded.');
-  });
-
-  // The beforeunload event is fired when the window, the document and its resources are about to be unloaded.
-  // addEventListener('beforeunload', (event) => {
-  //     // show the confirmation dialog
-  //     event.preventDefault();
-  //     // Google Chrome requires returnValue to be set.
-  //     event.returnValue = '';
-  // });
-
-  // The unload event is fired when the document or a child resource is being unloaded.
-  addEventListener('unload', (event) => {
-    // send analytic data
-  });
-
-  // add event listener to the logo
-  let mainIcon = document.querySelector("#mainIcon");
-
-  mainIcon.addEventListener('load', (event) => {
-    console.log('Logo has been loaded!');
-  });
-
-  // set the source of the logo
-  mainIcon.src = "images/Clear Vision.png";
-
-
   document.addEventListener('DOMContentLoaded', () => {
     const testimonialSection = document.querySelector('.testimonial-section');
     const testimonialContainer = testimonialSection.querySelector('.testimonial-container');
@@ -75,5 +26,24 @@ setTimeout(() => {
     autoScrollTestimonials();
   });
 
+  const mediaQuery = window.matchMedia("(max-width: 800px)");
+  function handleTabletChange(e) {
+    // Check if the media query is true
+    if (e.matches) {
+      // Then log enabled
+      // Change the text of the h1 element (you can change this to whatever element you want)
+      document.querySelector('#FCS').textContent = "FullClear";
+    } else {
+      // Change the text back to the original text
+      document.querySelector('#FCS').textContent = "FullClear Solutions";
+    }
+  }
 
+  // Add a listener for when the media query changes
+  mediaQuery.addEventListener("change", handleTabletChange);
+
+  // Call the function once to set the initial state
+  handleTabletChange(mediaQuery);
+
+  console.log("HELLO");
 
