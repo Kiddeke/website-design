745a5c90bc2776ca5dfaa56d087ab76aad74bf0c
grant <117955357+Kiddeke@users.noreply.github.com>
2024-09-30 21:22:46 -0400

    Update db and UI

--- public/script.js
+++ public/script.js
@@ -47,3 +47,11 @@
 
   console.log("HELLO");
 
+  import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kilkykmryiwkmahvfjdy.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+localStorage ()
+
