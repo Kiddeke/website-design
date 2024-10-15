81903fad4684bbc2f4bd66ac00a35d42ec2ccc60
grant <117955357+Kiddeke@users.noreply.github.com>
2024-10-15 13:15:07 -0400

    Update logo

--- public/script.js
+++ public/script.js
@@ -1,15 +1,13 @@
-
-
   const mediaQuery = window.matchMedia("(max-width: 800px)");
   function handleTabletChange(e) {
     // Check if the media query is true
     if (e.matches) {
       // Then log enabled
       // Change the text of the h1 element (you can change this to whatever element you want)
-      document.querySelector('#FCS').textContent = "FullClear";
+      document.querySelector('#FCS').innerHTML = '<img class="max-w-10" src="images/Clear Vision.png" alt="FCS" />FullClear';
     } else {
       // Change the text back to the original text
-      document.querySelector('#FCS').textContent = "FullClear Solutions";
+      document.querySelector('#FCS').innerHTML = '<img class="max-w-10" src="images/Clear Vision.png" alt="FCS" />FullClear Solutions';
     }
   }
 
